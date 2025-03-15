df6616e1866ca34b69429ca0fe407e80d99ba1bf
McOriginal <originalmc223@gmail.com>
2025-03-15 18:06:15 +0000

    Ajout de fichier

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+const menu_bar = document.querySelector('nav .menu');
+const close_menu = document.querySelector('nav .close');
+const nav_items = document.querySelector('.nav_items');
+
+// ---------------------------------------------------
+// -------------- nav_items Visibily -------------------------------------
+// ---------------------------------------------------
+menu_bar.addEventListener('click', () => {
+  nav_items.style.visibility = 'visible';
+  nav_items.style.opacity = 1;
+});
+close_menu.addEventListener('click', () => {
+  nav_items.style.visibility = 'hidden';
+  nav_items.style.opacity = 0;
+});
+
+// ---------------------------------------------------
+// -------------- nav_items Scrool Effet -------------------------------------
+// ---------------------------------------------------
+
+const navBar = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+  const scroll = window.scrollY;
+  navBar.style.transform = `translateY(${scroll}px)`;
+
+  if (scroll >= 40) {
+    navBar.classList.add('scrollbar');
+  } else {
+    navBar.classList.remove('scrollbar');
+  }
+});
