e01f1444fb34ef6782022d21d0ffde41e95006c6
McOriginal <originalmc223@gmail.com>
2025-03-16 15:22:17 +0000

    Mis ajour de index page

--- assets/script.js
+++ assets/script.js
@@ -30,3 +30,29 @@ window.addEventListener('scroll', () => {
     navBar.classList.remove('scrollbar');
   }
 });
+
+//  ------------------------------------------
+//  ------------ Galerie Slider ------------------------------
+//  ------------------------------------------
+//  ------------------------------------------
+
+var swiper = new Swiper('.mySwiper', {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper('.mySwiper2', {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+  autoplay: {
+    delay: 3000, // Délai en millisecondes entre les slides
+    disableOnInteraction: false, // Continue l'autoplay même après interaction utilisateur
+  },
+});
